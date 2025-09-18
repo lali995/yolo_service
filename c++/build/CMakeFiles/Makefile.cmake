4b9d877ddc4b00de99d579a8a1415a7bfcb97c6f
lali995 <yohannestaye83@gmail.com>
2025-09-18 10:23:16 +0000

    Add docker test image

--- c++/build/CMakeFiles/Makefile.cmake
+++ c++/build/CMakeFiles/Makefile.cmake
@@ -131,5 +131,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/yolo_image_processor.dir/DependInfo.cmake"
+  "CMakeFiles/yolo_test_headless.dir/DependInfo.cmake"
   "CMakeFiles/yolo_test.dir/DependInfo.cmake"
   )
